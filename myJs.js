242adc409435923fe4d8bcd162a0598090b8d76c
ChakerYaakoub <117543062+ChakerYaakoub@users.noreply.github.com>
2024-03-22 11:40:50 +0100

    2323

--- myJs.js
+++ myJs.js
@@ -1,48 +1,51 @@
 const http = "https://cheery-kheer-98f25b.netlify.app/.netlify/functions/api";
 // const http = "http://localhost:3000";
 
-const myBtnSubmit = async (e) => {
-  //e.preventDefault();
+const myBtnSubmit = (e) => {
+  e.preventDefault();
 
   const isLogin = localStorage.getItem("isLogin");
   const userId = localStorage.getItem("userId");
+  console.log("isLogin:", localStorage.getItem("userId"));
 
-  if (userId) {
-    try {
-      const response = await fetch("https://api.ipify.org?format=json");
-      if (!response.ok) {
-        throw new Error("Failed to fetch IP address");
-      }
-      const data = await response.json();
-      const userIp = data.ip;
-
-      const loginAttemptResponse = await fetch(`${http}/login-attempt`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ userIp, userId }),
-      });
-
-      if (loginAttemptResponse.ok) {
-        localStorage.setItem("isLogin", "true");
-        // Check if isLogin is set to true before redirecting
-        if (localStorage.getItem("isLogin") === "true") {
-          window.location.href = "./AfterLogin.html";
-        } else {
-          console.log("isLogin is not set to true yet");
+  if (localStorage.getItem("userId")) {
+    fetch("https://api.ipify.org?format=json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch IP address");
         }
-      } else {
-        console.log("Error:", loginAttemptResponse.statusText);
-      }
-    } catch (error) {
-      console.error("Error:", error);
-    }
+        return response.json();
+      })
+      .then((data) => {
+        const userIp = data.ip;
+
+        fetch(`${http}/login-attempt`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ userIp, userId }),
+        })
+          .then((response) => {
+            if (response.ok) {
+              localStorage.setItem("isLogin", "true");
+              window.location.href = "./AfterLogin.html";
+            } else {
+              console.log("Error:", response.statusText);
+            }
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
+      })
+      .catch((error) => {
+        console.error("Error fetching IP address:", error);
+      });
   } else {
+    // window.location.href = "./AfterLogin.html";
     console.log("User ID not found in local storage");
   }
 };
-
 // Function to send the user's IP address to the backend
 const sendIpToBackend = () => {
   const userId = localStorage.getItem("userId");
