d51abd3def5900a603602158e2a0f89c31baee1e
ChakerYaakoub <117543062+ChakerYaakoub@users.noreply.github.com>
2024-03-22 11:28:56 +0100

    test

--- myJs.js
+++ myJs.js
@@ -2,14 +2,12 @@ const http = "https://cheery-kheer-98f25b.netlify.app/.netlify/functions/api";
 // const http = "http://localhost:3000";
 
 const myBtnSubmit = (e) => {
-  e.preventDefault(); // To prevent the default button behavior
+  e.preventDefault();
 
-  // Check if user is logged in (isLogin is false)
   const isLogin = localStorage.getItem("isLogin");
   const userId = localStorage.getItem("userId");
 
   if (userId) {
-    // Fetch the IP address first
     fetch("https://api.ipify.org?format=json")
       .then((response) => {
         if (!response.ok) {
@@ -19,9 +17,7 @@ const myBtnSubmit = (e) => {
       })
       .then((data) => {
         const userIp = data.ip;
-        // console.log("User IP address:", userIp);
 
-        // Send the IP address along with the login attempt
         fetch(`${http}/login-attempt`, {
           method: "POST",
           headers: {
@@ -31,49 +27,36 @@ const myBtnSubmit = (e) => {
         })
           .then((response) => {
             if (response.ok) {
-              // If request successful, redirect
               localStorage.setItem("isLogin", "true");
               window.location.href = "./AfterLogin.html";
             } else {
               console.log("Error:", response.statusText);
-              // Handle error scenario as needed
             }
           })
           .catch((error) => {
             console.error("Error:", error);
-            // Handle network error scenario
-            // to delete
-            // window.location.href = "./AfterLogin.html";
           });
       })
       .catch((error) => {
         console.error("Error fetching IP address:", error);
-        // Handle error scenario if IP address fetching fails
       });
   } else {
-    // If user is already logged in, redirect to AfterLogin.html
     window.location.href = "./AfterLogin.html";
   }
 };
-
 // Function to send the user's IP address to the backend
 const sendIpToBackend = () => {
-  // Check if user ID is already stored in local storage
   const userId = localStorage.getItem("userId");
   if (userId) {
-    // console.log("User ID already exists in local storage:", userId);
-    //localStorage.removeItem("userId");
-
-    return; // Exit function early if user ID is already present
+    console.log("User ID already exists in local storage:", userId);
+    return;
   }
 
-  // Get the user's IP address using an external service
   fetch("https://api.ipify.org?format=json")
     .then((response) => response.json())
     .then((data) => {
       const userIp = data.ip;
 
-      // Send the IP address to the backend
       fetch(`${http}/open-site`, {
         method: "POST",
         headers: {
@@ -84,7 +67,6 @@ const sendIpToBackend = () => {
         .then((response) => response.json())
         .then((responseData) => {
           if (responseData.userId) {
-            // Store the received user ID in local storage
             localStorage.setItem("userId", responseData.userId);
             localStorage.setItem("isLogin", false);
             console.log(
@@ -97,16 +79,13 @@ const sendIpToBackend = () => {
         })
         .catch((error) => {
           console.error("Error sending IP address:", error);
-          // Handle network error scenario
         });
     })
     .catch((error) => {
       console.error("Error fetching IP address:", error);
-      // Handle error scenario as needed
     });
 };
 
-// Trigger the function to send the IP address when the page loads
 window.onload = function () {
-  sendIpToBackend(); // Call the function to send the IP address to the backend
+  sendIpToBackend();
 };
